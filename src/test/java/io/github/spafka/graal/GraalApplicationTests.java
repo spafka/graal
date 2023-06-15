0d2cd9abbdb85c1ec8804bed11aa87aa2a34232d
spafka <spafka@163.com>
2023-06-15 23:28:36 +0800

    first commit

---
+++ src/test/java/io/github/spafka/graal/GraalApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.spafka.graal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
